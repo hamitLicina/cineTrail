ceb9408583f7179c7102d2dbae628593ce35af43
hamitLicina <hamitlicina@gmail.com>
2023-08-31 07:09:11 +0300

    Cine Trail Project Initialized

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        <h1>Hello React</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
